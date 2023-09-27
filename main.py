1307e05290a18d0445df3495c200184e56be28d7
Daniele Zago - ST <zagodaniele.9@gmail.com>
2023-09-27 18:40:02 -0400

    Working test
    Co-authored-by: Nicola Davanzo <Neeqstock@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,63 +1,71 @@
+#!/usr/bin/python3
 import openai
 import pyperclip
 import os
 import platform
 import simpleaudio as sa
 from pynput import keyboard
-
-# Funzioni del cazzo di pynput porcoddio
-def on_press(key):
-    print(f'{key} pressed')
-
-def on_release(key):
-    print(f'{key} release')
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-# Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+import json
 
 
-def chat_with_gpt(prompt):
-    # Define the system message
-    system_msg = 'You are a helpful assistant.'
-
-    # Create a dataset using GPT
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[{"role": "system", "content": system_msg},
-                                            {"role": "user", "content": prompt}])
-
-    return response["choices"][0]["message"]["content"]
-
+global global_response
 
 # OpenAI key
 api_key = "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"
 # Initialize the OpenAI API client
 openai.api_key = api_key
 
-        
+
+def on_key_press(key):
+    global global_response
+    global_response = chat_with_gpt(key.char)
+    return False
+
+
+def intercept_next_keystroke():
+    # Create a keyboard listener
+    listener = keyboard.Listener(on_press=on_key_press)
+
+    # Start listening for the next keystroke
+    with listener:
+        listener.join()
+
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def chat_with_gpt(prompt_number):
+    # Define the system message
+    user_input = pyperclip.paste()                          # Get string from clipboard
+    settings = read_json_file("prompts/" + str(prompt_number) + ".json")
+
+    mergedPrompt = settings["prompt"].replace('§', user_input)
+
+    print(settings)
+
+    # Create a dataset using GPT
+    response = openai.ChatCompletion.create(model=settings["gptModel"],
+                                            messages=[{"role": "system", "content": settings["systemMessage"]},
+                                            {"role": "user", "content": mergedPrompt}])
+    print(response)
+
+    return response["choices"][0]["message"]["content"]
+
+
 def play_completion_sound():
     wave_obj = sa.WaveObject.from_wave_file('completed.wav')
     play_obj = wave_obj.play()
     play_obj.wait_done()  # Wait for the sound to finish playing
 
 
-
 def main():
-    
-    while True:
-        input("Press Enter to read from clipboard...")
-        user_input = pyperclip.paste()                          # Get string from clipboard
-
-        if user_input.lower() == 'exit':
-            break
-
-        response = chat_with_gpt(f"You: {user_input}\nAI:")     # Run ChatGPT
-        print(response)
-        pyperclip.copy(response)                                # Save output to clipboard
-        play_completion_sound()
+    print("Write a keystroke to be captured by the script...")
+    intercept_next_keystroke()
+    pyperclip.copy(global_response)                                # Save output to clipboard
+    play_completion_sound()
 
 
 if __name__ == "__main__":
