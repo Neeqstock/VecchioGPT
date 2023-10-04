9043fe2b3da502fda83cd21dbdbbb32a64d7112e
Neeqstock <nicola.davanzo01@gmail.com>
2023-10-05 01:49:18 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -11,177 +11,204 @@ import sv_ttk
 from PIL import ImageTk, Image
 import codecs
 
-# Directory containing the prompts
-promptsDirectoryName = os.path.join(os.path.dirname(__file__), 'prompts')
+global data
+
+promptsDirectoryName = os.path.join(os.path.dirname(__file__), 'prompts') # Directory containing the prompts
 promptsDictionary = {}
 possible_prompts = []  # List of possible prompts
 selected_index = -1  # Currently selected item index
 selected_item = -1
 tempPromptName = ""
+key_entry_pairs = {} # A dictionary containing parameters keys mapped to tkinter Entries (from the notebook)
 
 def bring_to_front(window):
-    window.lift()
-    window.attributes('-topmost', True)
-    window.after_idle(window.attributes, '-topmost', False)
+	window.lift()
+	window.attributes('-topmost', True)
+	window.after_idle(window.attributes, '-topmost', False)
+	
 
 def on_search(event):
-    global selected_index
-    search_text = entry.get()
-    if search_text:
-        # Use fuzzy matching to find similar prompts
-        matches = process.extract(search_text, possible_prompts, scorer=fuzz.token_sort_ratio, limit=5)
-        listbox.delete(0, tk.END)
-        for match, score in matches:
-            listbox.insert(tk.END, match)
-        selected_index = -1  # Reset selected index
+	global selected_index
+	search_text = entry.get()
+	if search_text:
+		# Use fuzzy matching to find similar prompts
+		matches = process.extract(search_text, possible_prompts, scorer=fuzz.token_sort_ratio, limit=5)
+		listbox.delete(0, tk.END)
+		for match, score in matches:
+			listbox.insert(tk.END, match)
+		selected_index = -1  # Reset selected index
+		
 
 def on_tab(event):
-    if listbox.size() > 0:
-        global selected_index
-        listbox.focus_set()
-        listbox.selection_clear(0, tk.END)
-        listbox.selection_set(selected_index)
-        listbox.see(selected_index)  # Scroll to the selected index
+	if listbox.size() > 0:
+		global selected_index
+		listbox.focus_set()
+		listbox.selection_clear(0, tk.END)
+		listbox.selection_set(selected_index)
+		listbox.see(selected_index)  # Scroll to the selected index
+		
+def overwrite_additionalParams():
+	for i in range(len(data["additionalParams"])):
+		key = data["additionalParams"][i]["key"]
+		data["additionalParams"][i]["value"] = key_entry_pairs[key].get()
+	fn = promptsDictionary.get(selected_item)
+	# Seeks the file name
+	fileName = os.path.join(os.path.dirname(__file__), "prompts/" + fn)
+	with open(fileName, "w") as file:
+		json.dump(data, file, indent=4, ensure_ascii=False)
 
 def on_select(event):
-    if listbox.curselection():
-        global selected_index
-        global selected_item
-        
-        print(f"Selected Prompt: {selected_item}")
-        entry.delete(0, tk.END)
-        entry.insert(0, selected_item)
-        global global_response
-        functions.play_sound(functions.SOUND_START)
-        tempPromptName = selected_item  # Store the selected item so it can destroy the root window
-        root.destroy()
-        global_response = functions.chat_with_gpt(promptsDictionary.get(tempPromptName))
-        pyperclip.copy(global_response)
-        functions.play_sound(functions.SOUND_COMPLETED)
+    global selected_index
+    global selected_item
+
+	# Overwrite additional params to set new defaults
+    if len(key_entry_pairs) > 0:
+        overwrite_additionalParams()
+    
+    print(f"Selected Prompt: {selected_item}")
+    entry.delete(0, tk.END)
+    entry.insert(0, selected_item)
+    global global_response
+    functions.play_sound(functions.SOUND_START)
+    tempPromptName = selected_item  # Store the selected item so it can destroy the root window
+    root.destroy()
+    global_response = functions.chat_with_gpt(promptsDictionary.get(tempPromptName))
+    pyperclip.copy(global_response)
+    functions.play_sound(functions.SOUND_COMPLETED)
+
 
 def on_enter(event):
-    if selected_index >= 0:
-        on_select(event)
+	print(f"selected_index: {selected_index}\n")
+	print(f"selected_item: {selected_item}\n")
+	if selected_index >= 0:
+		on_select(event)
+		
 
 def on_up_arrow(event):
-    global selected_index
-    global selected_item
-    if entry == root.focus_get():
-        if listbox.size() > 0:
-            selected_index = 0
-            listbox.selection_clear(0, tk.END)
-            listbox.selection_set(selected_index)
-            listbox.activate(selected_index)
-            selected_index = listbox.curselection()[0]
-            selected_item = listbox.get(selected_index)
-            display_info()
-        else:
-            selected_index = -1
-    elif selected_index > 0:
-        selected_index -= 1
-        listbox.selection_clear(0, tk.END)
-        listbox.selection_set(selected_index)
-        listbox.activate(selected_index)
-        selected_index = listbox.curselection()[0]
-        selected_item = listbox.get(selected_index)
-        display_info()
+	global selected_index
+	global selected_item
+	if entry == root.focus_get():
+		if listbox.size() > 0:
+			selected_index = 0
+			listbox.selection_clear(0, tk.END)
+			listbox.selection_set(selected_index)
+			listbox.activate(selected_index)
+			selected_index = listbox.curselection()[0]
+			selected_item = listbox.get(selected_index)
+			display_info()
+		else:
+			selected_index = -1
+	elif selected_index > 0:
+		selected_index -= 1
+		listbox.selection_clear(0, tk.END)
+		listbox.selection_set(selected_index)
+		listbox.activate(selected_index)
+		selected_index = listbox.curselection()[0]
+		selected_item = listbox.get(selected_index)
+		display_info()
+		
 
 def on_down_arrow(event):
-    global selected_index
-    global selected_item
-    if entry == root.focus_get():
-        if listbox.size() > 0:
-            selected_index = 0
-            listbox.selection_clear(0, tk.END)
-            listbox.selection_set(selected_index)
-            listbox.activate(selected_index)
-            selected_index = listbox.curselection()[0]
-            selected_item = listbox.get(selected_index)
-            display_info()
-        else:
-            selected_index = -1
-    elif selected_index < listbox.size() - 1:
-        selected_index += 1
-        listbox.selection_clear(0, tk.END)
-        listbox.selection_set(selected_index)
-        listbox.activate(selected_index)
-        selected_index = listbox.curselection()[0]
-        selected_item = listbox.get(selected_index)
-        display_info()
+	global selected_index
+	global selected_item
+	if entry == root.focus_get():
+		if listbox.size() > 0:
+			selected_index = 0
+			listbox.selection_clear(0, tk.END)
+			listbox.selection_set(selected_index)
+			listbox.activate(selected_index)
+			selected_index = listbox.curselection()[0]
+			selected_item = listbox.get(selected_index)
+			display_info()
+		else:
+			selected_index = -1
+	elif selected_index < listbox.size() - 1:
+		selected_index += 1
+		listbox.selection_clear(0, tk.END)
+		listbox.selection_set(selected_index)
+		listbox.activate(selected_index)
+		selected_index = listbox.curselection()[0]
+		selected_item = listbox.get(selected_index)
+		display_info()
+
 
 def clear_notebook(notebook):
-    for tab in notebook.winfo_children():
-        tab.destroy()
-    for i in range(notebook.index("end") - 1, -1, -1):
-        notebook.forget(i)
+	for tab in notebook.winfo_children():
+		tab.destroy()
+	for i in range(notebook.index("end") - 1, -1, -1):
+		notebook.forget(i)
 
 
-
-                        
 def display_info():
-    global selected_index
-    if selected_index >= 0:
-        selected_item = listbox.get(selected_index)
-        filename = promptsDictionary.get(selected_item)
-        # Clear notebook
-        clear_notebook(notebook)
+	global selected_index
+	global data
+	if selected_index >= 0:
+		selected_item = listbox.get(selected_index)
+		filename = promptsDictionary.get(selected_item)
+		# Clear notebook
+		clear_notebook(notebook)
+		key_entry_pairs.clear()
 
-        if filename:
-            try:
-                with open(os.path.join(promptsDirectoryName, filename)) as f:
-                    data = json.load(f)
-                    # Decode Unicode escape sequences to display proper Unicode characters
-                    decoded_info = codecs.decode(json.dumps(data, indent=2), 'unicode_escape')
-                    
-                    # Show only prompt description
-                    info_text = data['description']
-                    # info_text = f"[{data['language']}] {data['promptName']} Info:\n\n{decoded_info}"
-                    info_label.configure(text=info_text)
+		if filename:
+			try:
+				with open(os.path.join(promptsDirectoryName, filename)) as f:
+					data = json.load(f)
+					# Decode Unicode escape sequences to display proper Unicode characters
+					decoded_info = codecs.decode(json.dumps(data, indent=2), 'unicode_escape')
+					
+					# Show only prompt description
+					info_text = data['description']
+					# info_text = f"[{data['language']}] {data['promptName']} Info:\n\n{decoded_info}"
+					info_label.configure(text=info_text)
 
-                    additional_params = data.get("additionalParams")
+					additional_params = data.get("additionalParams")
 
-                    if additional_params:
-                        # Create tabs for each key in "additionalParams"
-                        # Iterate through each key-value pair in "additionalParams"
-                        for param in additional_params:
-                            key = param.get("key")
-                            value = param.get("value")
+					if additional_params:
+						# Create tabs for each key in "additionalParams"
+						# Iterate through each key-value pair in "additionalParams"
+						for param in additional_params:
+							key = param.get("key")
+							value = param.get("value")
 
-                            # Create a new tab
-                            tab = ttk.Frame(notebook)
-                            notebook.add(tab, text=key)
+							# Create a new tab
+							tab = ttk.Frame(notebook)
+							notebook.add(tab, text=key)
 
-                            # Create an InputBox in the tab
-                            input_box = ttk.Entry(tab, width=80, font=("Montserrat", 12))
-                            input_box.bind("<Return>", on_enter)
-                            input_box.insert(0, value)  # Set default text
-                            input_box.pack(pady=5)
+							# Create an InputBox in the tab
+							input_box = ttk.Entry(tab, width=80, font=("Montserrat", 12))
+							input_box.bind("<Return>", on_enter)
+							input_box.insert(0, value)  # Set default text
+							input_box.pack(pady=5)
+							key_entry_pairs[key] = input_box
 
-            except FileNotFoundError:
-                info_label.configure(text=f"Info not available for {selected_item}")
-        else:
-            info_label.configure(text=f"Info not available for {selected_item}")
+			except FileNotFoundError:
+				info_label.configure(text=f"Info not available for {selected_item}")
+		else:
+			info_label.configure(text=f"Info not available for {selected_item}")
 
 def show_window():
-    bring_to_front(root)
-    root.mainloop()
+	bring_to_front(root)
+	root.mainloop()
 
-# Populate the prompts list =================
+
+# GUI ENTRY CODE =========================================================================================
+
+# Populate the prompts list
 for filename in os.listdir(promptsDirectoryName):
-    # Check if the file is a .json file
-    if filename.endswith('.json'):
-        # Open the .json file
-        with open(f'{promptsDirectoryName}/{filename}') as f:
-            # Load the JSON data from the file
-            data = json.load(f)
-            # Strcat with prompt language
-            promptString = "[" + data['language'] + "] " + data['promptName']
-            # Append the 'promptName' and filename to the dictionary
-            promptsDictionary[promptString] = filename
-            # Append the 'promptName' to the list to be used by the GUI
-            possible_prompts.append(promptString)
+	# Check if the file is a .json file
+	if filename.endswith('.json'):
+		# Open the .json file
+		with open(f'{promptsDirectoryName}/{filename}') as f:
+			# Load the JSON data from the file
+			data = json.load(f)
+			# Strcat with prompt language
+			promptString = "[" + data['language'] + "] " + data['promptName']
+			# Append the 'promptName' and filename to the dictionary
+			promptsDictionary[promptString] = filename
+			# Append the 'promptName' to the list to be used by the GUI
+			possible_prompts.append(promptString)
 
-# Define the window ====================
+# Define the window
 # Create the main window
 root = tk.Tk()
 root.title("VecchioGPT")
@@ -214,7 +241,7 @@ label.pack()
 entry = ttk.Entry(root, width=80, font=("Montserrat", 12))
 entry.pack(pady=5)
 entry.bind("<KeyRelease>", on_search)
-entry.bind("<Tab>", on_tab)
+# entry.bind("<Tab>", on_tab)
 entry.focus_set()  # Set focus on the input text box
 # Add space between Notebooks and Textbox
 entry.pack(pady=(5, 5))
@@ -222,7 +249,7 @@ entry.pack(pady=(5, 5))
 
 # Create and set the listbox with an adjusted width and borderless selection
 listbox = tk.Listbox(root, selectmode=tk.SINGLE, height=5, width=80,
-                     font=("Montserrat", 12), bd=0, highlightthickness=0)
+					 font=("Montserrat", 12), bd=0, highlightthickness=0)
 listbox.pack(pady=5)
 listbox.bind("<Return>", on_enter)
 listbox.bind("<Up>", on_up_arrow)
@@ -233,7 +260,7 @@ listbox.pack(pady=(5, 5))
 
 # Populate the listbox with possible prompts
 for prompt in possible_prompts:
-    listbox.insert(tk.END, prompt)
+	listbox.insert(tk.END, prompt)
 
 sv_ttk.set_theme("dark")
 
@@ -247,4 +274,4 @@ notebook.pack(pady=5)
 
 # Main entry point ===================================
 if __name__ == "__main__":
-    show_window()
+	show_window()
